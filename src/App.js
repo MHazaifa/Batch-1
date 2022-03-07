39484c8eb83dc928b1000ea940e79ed086f60086
Muhmmad Hazaifa <Mhazaifa>
2022-03-07 22:35:29 +0500

    Api fetch

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
-
 import './App.css';
-
 import {useState} from 'react';
-import Home from './Home';
+import axios from 'axios';
+
 
 
 function App() {
- 
+  const [city, setCity] = useState("Lahore");
+  const [weather, setWeather] = useState("");
+
+  const Behaviour = (e) => {
+    e.preventDefault();
+
+  const axios = require("axios");
+    axios
+      .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'26691ef000ffff049bef8b9e4e4e2ce5'}`)
+      .then(function (response) {
+        console.log(response);
+        setWeather({
+          temp : ((response.data.main.temp - 273.15).toFixed(0)),
+          maxi : ((response.data.main.temp_max- 273.15).toFixed(0)),
+          mini : ((response.data.main.temp_min- 273.15).toFixed(0)),
+        })
+      })
+      .catch(function (error) {
+      console.log(error);
+      })
+      .then(function () {
+      
+      });
+      
+    };
   return (
     <div>
-     
-      <Home/>
+      <h1>{city}</h1>
+      <form onSubmit={Behaviour}>
+      <input type="text" value={city} onChange={(e)=>setCity(e.target.value)}/>
+     <input type="submit" value="Search"/>
+      </form>
+    <h1> Temp : {weather.temp}</h1>
+    <h2> Maximum : {weather.maxi}</h2>
+    <h3> Minimm : {weather.mini}</h3>
+
     </div>
   );
 }
   
-//   const styleCondition3 = {
-   
-//     color : 'red',
-//     backgroundColor : 'blue'
-//    }
-//    const dynamicStyle2 = {
-   
-//     color : 'yellow',
-//     backgroundColor : 'red'
-//    }
-//  const styleCondition = true;
- 
-//   //   const str = 'huz';
-//   // const num = 4 + 5;
-//   // const obj = {name: 'huzaifa', age: '23'};
 
-//   return(
-//     <h1 style={styleCondition ? dynamicStyle2 : styleCondition3}>Hazaifa</h1>
-//   );
-// }
-//    if(styleCondition){
-//      return (
-//        <div>
-//          <h1 style={{backgroundColor : "blue"}}>Hazaifa</h1>
-//          <h3 style={styleCondition ? styleCondition3 : styleCondition3}>Hassan</h3>
-//        </div>
-//      );
-//    }
-//     else{
-//       return (
-//         <div>
-//           <h2>Awan</h2>
-//         </div>
-//       );
-//     }
-  
 
 export default App;
---
+++ src/components/Api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import React from 'react'
+
+const Api = () => {
+    
+  return (
+    <div>Api</div>
+  )
+}
+
+export default Api
